bfd8310d7a6d78a855bc04fb71332bea24b2ceec
waleedkhaledalghool <119655482+waleedkhaledalghool@users.noreply.github.com>
2024-08-17 10:13:53 +0300

    Added the Arduino code

---
+++ arduino_code.ino
@@ -0,0 +1,186 @@
+#include <Servo.h>          //Servo motor library. This is standard library
+#include <NewPing.h>        //Ultrasonic sensor function library. You must install this library
+
+//our L298N control pins
+
+const int LeftMotorForward = A0;
+const int LeftMotorBackward = A1;
+const int RightMotorForward = A2;
+const int RightMotorBackward = A3;
+
+//sensor pins
+#define S1 6 // pin 7 for Ir sensor
+#define S2 5 // pin 6 for Ir sensor
+#define S3 4 // pin 5 for Ir sensor
+#define S4 3 // pin 4 for Ir sensor
+#define trig_pin A5 //analog input 1
+#define echo_pin A4 //analog input 2
+
+#define maximum_distance 200
+boolean goesForward = false;
+int distance = 100;
+
+NewPing sonar(trig_pin, echo_pin, maximum_distance); //sensor function
+Servo servo_motor; //our servo name
+
+
+void setup(){
+
+  pinMode(RightMotorForward, OUTPUT);
+  pinMode(LeftMotorForward, OUTPUT);
+  pinMode(LeftMotorBackward, OUTPUT);
+  pinMode(RightMotorBackward, OUTPUT);
+  pinMode(S1, INPUT);//define front-left input pin
+  pinMode(S2, INPUT);//define front-right input pin
+  pinMode(S3, INPUT);//define rear-left input pin
+  pinMode(S4, INPUT);//define rear-right input pin 
+  servo_motor.attach(7); //our servo pin
+
+  servo_motor.write(90);
+  delay(2000);
+  distance = readPing();
+  delay(100);
+  distance = readPing();
+  delay(100);
+  distance = readPing();
+  delay(100);
+  distance = readPing();
+  delay(100);
+}
+
+void loop(){
+  
+ int sens1 = digitalRead(S1);
+ int sens2 = digitalRead(S2);
+ int sens3 = digitalRead(S3);
+ int sens4 = digitalRead(S4);
+  
+  int distanceRight = 0;
+  int distanceLeft = 0;
+  delay(50);
+
+  if (distance <= 50 ||sens1 == LOW || sens2 == LOW || sens3== LOW ||sens4 == LOW ){
+    moveStop();
+    delay(300);
+    moveBackward();
+    delay(600);
+    moveStop();
+    delay(300);
+    distanceRight = lookRight();
+    delay(300);
+    distanceLeft = lookLeft();
+    delay(300);
+
+    if (distanceRight >= distanceLeft){
+      turnRight();
+      moveStop();
+    }
+    else{
+      turnLeft();
+      moveStop();
+    }
+  }
+  else{
+    moveForward(); 
+  }
+    distance = readPing();
+}
+
+int lookRight(){  
+  servo_motor.write(30);
+  delay(500);
+  int distance = readPing();
+  delay(100);
+  servo_motor.write(90);
+  return distance;
+}
+
+int lookLeft(){
+  servo_motor.write(150);
+  delay(500);
+  int distance = readPing();
+  delay(100);
+  servo_motor.write(90);
+  return distance;
+  delay(100);
+}
+
+int readPing(){
+  delay(70);
+  int cm = sonar.ping_cm();
+  if (cm==0){
+    cm=250;
+  }
+  return cm;
+}
+
+void moveStop(){
+  
+  digitalWrite(RightMotorForward, LOW);
+  digitalWrite(LeftMotorForward, LOW);
+  digitalWrite(RightMotorBackward, LOW);
+  digitalWrite(LeftMotorBackward, LOW);
+}
+
+void moveForward(){
+
+  if(!goesForward){
+
+    goesForward=true;
+    
+    digitalWrite(LeftMotorForward, HIGH);
+    digitalWrite(RightMotorForward, HIGH);
+  
+    digitalWrite(LeftMotorBackward, LOW);
+    digitalWrite(RightMotorBackward, LOW); 
+  }
+}
+
+void moveBackward(){
+
+  goesForward=false;
+
+  digitalWrite(LeftMotorBackward, HIGH);
+  digitalWrite(RightMotorBackward, HIGH);
+  
+  digitalWrite(LeftMotorForward, LOW);
+  digitalWrite(RightMotorForward, LOW);
+  
+}
+
+void turnRight(){
+
+  digitalWrite(LeftMotorForward, HIGH);
+  digitalWrite(RightMotorBackward, HIGH);
+  
+  digitalWrite(LeftMotorBackward, LOW);
+  digitalWrite(RightMotorForward, LOW);
+  
+  delay(450);
+  
+  digitalWrite(LeftMotorForward, HIGH);
+  digitalWrite(RightMotorForward, HIGH);
+  
+  digitalWrite(LeftMotorBackward, LOW);
+  digitalWrite(RightMotorBackward, LOW);
+ 
+  
+  
+}
+
+void turnLeft(){
+
+  digitalWrite(LeftMotorBackward, HIGH);
+  digitalWrite(RightMotorForward, HIGH);
+  
+  digitalWrite(LeftMotorForward, LOW);
+  digitalWrite(RightMotorBackward, LOW);
+
+  delay(450);
+  
+  digitalWrite(LeftMotorForward, HIGH);
+  digitalWrite(RightMotorForward, HIGH);
+  
+  digitalWrite(LeftMotorBackward, LOW);
+  digitalWrite(RightMotorBackward, LOW);
+}
